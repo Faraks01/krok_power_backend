09332d25a24ea91cc82a4b1ba421dc73c2589212
Konstantin Nikolaev <faraks01@gmail.com>
2023-12-11 19:12:14 +0300

    Updated run_prod.sh script

--- run_prod.sh
+++ run_prod.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ -z "${EMAIL_HOST_USER}" ]]; then
+  echo "Enter an email host user"
+  read EMAIL_HOST_USER
+fi
+
+if [[ -z "${EMAIL_HOST_PASSWORD}" ]]; then
+  echo "Enter an email host user"
+  read EMAIL_HOST_PASSWORD
+fi
+
 # Stopping running server instance if exists
 PID=$(lsof -t -i:8000)
 
@@ -10,4 +20,4 @@ fi
 
 # Run server at background
 echo "Starting server on 0.0.0.0:8000"
-DEBUG=False nohup python3 manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1&
+DEBUG=False EMAIL_HOST_USER=$EMAIL_HOST_USER EMAIL_HOST_PASSWORD=$EMAIL_HOST_PASSWORD nohup python3 manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1&
